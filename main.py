56e9453076020f87bcd226273b554998bfdc5733
Baraa Al-Masri <m.baraa.masri@gmail.com>
2023-08-06 14:37:48 +0300

    chore: removed unnecessary global API_TOKEN variable

--- main.py
+++ main.py
@@ -13,16 +13,12 @@ app = FastAPI(
     description="API to serve ML model for hate-speech classification",
 )
 
-API_TOKEN = ""
-
 @app.on_event("startup")
 def on_startup():
     """Load model on startup."""
     model_path = os.environ.get("MODEL_PATH")
     model_version = os.environ.get("MODEL_VERSION")
     huggingface_token = os.environ.get("HUGGINGFACE_TOKEN")
-    global API_TOKEN
-    API_TOKEN = os.environ.get("API_TOKEN")
 
     huggingface_hub.login(token=huggingface_token)
     app.state.model = Nuha(model_path=model_path, model_version=model_version)
@@ -34,7 +30,7 @@ async def predict(
         response: Response
 ) -> list[PredictionResponse]:
     """check for valid API token first."""
-    if len(authorization) == 0 or authorization[len("Bearer "):] != API_TOKEN:
+    if len(authorization) == 0 or authorization[len("Bearer "):] != os.environ.get("API_TOKEN"):
         response.status_code = status.HTTP_401_UNAUTHORIZED
         return []
 
